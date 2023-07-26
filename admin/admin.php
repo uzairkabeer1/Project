d8f04351b1d746c9372b43007b2936497e594fef
Muhammad Uzair kabeer <muhammaduzairkabeer@gmail.com>
2023-07-26 17:43:15 +0500

    added herouku database configuration

--- admin/admin.php
+++ admin/admin.php
@@ -2,8 +2,15 @@
 function getUsers() {
 
     try {
-        $conn = mysqli_connect("localhost", "root", "", "api_crud_db");
-
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
         $sql = "SELECT * FROM users";
         $users = mysqli_query($conn, $sql);
 
--- register/register.php
+++ register/register.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
 // Connect to the MySQL database
-$conn = mysqli_connect("localhost", "root", "", "api_crud_db");
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
