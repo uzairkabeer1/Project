19ad3f886df43d56e6dfe9aa80266638621d3217
Muhammad Uzair kabeer <muhammaduzairkabeer@gmail.com>
2023-07-26 17:51:30 +0500

    Update navbar.php

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,23 +1,23 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/quotes/index.php"><strong>Get Inspired</strong></a>
+        <a class="navbar-brand" href="../index.php"><strong>Get Inspired</strong></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/quotes/index.php">Home</a>
+                    <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/quotes/favourites/favourites_page.php">Favourites</a>
+                    <a class="nav-link" href="/favourites/favourites_page.php">Favourites</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/quotes/user/account_page.php">Account</a>
+                    <a class="nav-link" href="/user/account_page.php">Account</a>
                 </li>
             </ul>
             <button class="btn btn-sm btn-danger">
-                <a href="/quotes/logout.php" style="text-decoration: none; color: white;">Logout</a>
+                <a href="../logout.php" style="text-decoration: none; color: white;">Logout</a>
             </button>
         </div>
     </div>
